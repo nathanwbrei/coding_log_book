1dd648ff937f53138ea1828c8a515bbac1014121
Nathan Brei <nbrei@jlab.org>
2020-04-11 13:22:33 -0400

    Stating a clearer, simpler problem

--- main.cpp
+++ main.cpp
@@ -69,63 +69,21 @@ template <typename T> struct JFactoryT : public JFactory {
     };
 };
 
-template <typename B, typename D>
-std::vector<B*> get_downcasted(JFactoryT<D>* f) {
-    auto input = f->get();
-    std::vector<B*> output;
-    for (auto i : input) {
-        output.push_back(static_cast<B*>(output));
-    }
-    return output;
-}
-
-
-struct JEvent {
-    std::map<std::type_index, JFactory*> factories;
-
-    template <typename T>
-    void put(T* item) {
-        auto ti = std::type_index(typeid(T));
-        auto f = new JFactoryT<T>();
-        f->insert(item);
-        factories.insert(std::make_pair(ti, f));
-    }
-
-    template <typename T>
-    const std::vector<T*>& get() {
-        auto ti = std::type_index(typeid(T));
-        auto f = factories[ti];
-        auto tf = static_cast<JFactoryT<T>*>(f);
-        return tf->get();
-    }
-
-    template <typename S>
-    const std::vector<S*> getAll() {
-        JFactoryVisitor visitor;
-        std::vector<S*> results;
-        for (auto p: factories) {
-            auto f = p.second;
-            p.second->accept(visitor);
-        }
-        return visitor.bases;
-    }
-
-};
-
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    JEvent event;
-    event.put(new BaseData {3.0});
-    event.put(new DerivedData {7.0, 2});
-    event.put(new UnrelatedData {7.0});
+    JFactoryT<DerivedData> fdd;
+    fdd.insert(new DerivedData {7.0, 2});
 
-    //auto output = event.get<DerivedData>();
-    //std::cout << output[0]->i << std::endl;
+    JFactoryT<UnrelatedData> fud;
+    fud.insert(new UnrelatedData {9.0});
 
-    auto all = event.getAll<BaseData>();
-    for (auto item : all) {
+
+    JFactory* factory = static_cast<JFactory*>(&fdd);
+
+    auto output = factory.get_as<BaseData>();
+    for (auto item : output) {
         std::cout << item->d << std::endl;
     }
     return 0;
