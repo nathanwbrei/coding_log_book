2b5e097b9f37f2578c957db51ae70eb249d4ddf1
Nathan Brei <nbrei@jlab.org>
2020-04-12 09:13:31 -0400

    Progress with manual vtable. Problem: member fn pointers are not actually pointers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <typeinfo>
 #include <typeindex>
+#include <any>
 
 struct BaseData {
     double d;
@@ -20,19 +21,23 @@ struct UnrelatedData {
 
 class JFactory;
 
-// template<typename S>
-// std::vector<S*> free_get_as(JFactory* fac);
 
 template <typename T> class JFactoryT;
 
-template<typename S, typename T>
-void free_get_as(JFactoryT<T>* in, std::Vector<S*>& out);
-
 struct JFactory {
+    std::unordered_map<std::type_index, const void*> vtable;
+
     template<typename S>
     const std::vector<S*> get_as() {
         std::vector<S*> results;
-        free_get_as<S>(this, results);
+        auto ti = std::type_index(typeid(S));
+        auto search = vtable.find(ti);
+        if (search != vtable.end()) {
+            const void* lambda = search->second;
+            //std::any_cast();
+            // Need to cast a raw (untyped) fn pointer back to a method
+        }
+        return results;
     }
 };
 
@@ -57,32 +62,43 @@ template <typename T> struct JFactoryT : public JFactory {
         }
         return results;
     };
+
+    template <typename S>
+    void allow_cast_to() {
+        // https://isocpp.org/wiki/faq/pointers-to-members#fnptr-vs-memfnptr-types
+        std::cout << "Adding conversion to " << typeid(S).name() << std::endl;
+
+        typedef const std::vector<S*> (JFactoryT<T>::*CastToSFn)() const;
+
+        CastToSFn fn = &JFactoryT<T>::get_as<S>;
+        auto key = std::type_index(typeid(S));
+        vtable[key] = reinterpret_cast<void*>(fn);
+    }
 };
 
-// template<typename S>
-// std::vector<S*> free_get_as(JFactory* fac) {
-//     std::cout << "Matched untyped" << std::endl;
-// }
-template<typename S, typename T>
-std::vector<S*> free_get_as(JFactoryT<T>* fac) {
-    std::cout << "Matched TYPED" << std::endl;
-}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     JFactoryT<DerivedData> fdd;
     fdd.insert(new DerivedData {7.0, 2});
+    fdd.allow_cast_to<DerivedData>();
 
-    JFactoryT<UnrelatedData> fud;
-    fud.insert(new UnrelatedData {9.0});
-
-
-    JFactory* factory = static_cast<JFactory*>(&fdd);
-
-    auto output = factory->get_as<BaseData>();
-    for (auto item : output) {
-        std::cout << item->d << std::endl;
-    }
-    return 0;
+    //
+    // JFactoryT<UnrelatedData> fud;
+    // fud.insert(new UnrelatedData {9.0});
+    //
+    //
+    // JFactory* factory = static_cast<JFactory*>(&fdd);
+    //
+    // auto output = factory->get_as<BaseData>();
+    // for (auto item : output) {
+    //     std::cout << item->d << std::endl;
+    // }
+    //
+    // auto f = [] (int x) {return x + 1; };
+    // std::cout << f(22) << std::endl;
+    // std::any f_erased (f);
+    // auto g = std::any_cast<int(int)>(f_erased);
+    // return 0;
 }
