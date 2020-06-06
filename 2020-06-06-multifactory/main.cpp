feb70a3b51e96eb2cdaec6a39bca4514fe227330
Nathan Brei <nbrei@jlab.org>
2020-06-06 16:00:41 -0400

    Get constructor working

--- 2020-06-06-multifactory/main.cpp
+++ 2020-06-06-multifactory/main.cpp
@@ -5,19 +5,18 @@ template <typename... Ts>
 struct Multifactory {
 };
 
-
 /// Specialization for non-empty template parameters
 template <typename T, typename... Ts>
 struct Multifactory<T, Ts...> : public Multifactory<Ts...> {
 	T data;
+
+	explicit Multifactory(T t, Ts... ts) : Multifactory<Ts...>(ts...), data(t) {}
 };
 
 int main() {
 	std::cout << "Hello world from multifactory" << std::endl;
-	Multifactory<int, double, char> f;
-	f.data = 22;
-	static_cast<Multifactory<double, char>>(f).data = 23.0;
-	static_cast<Multifactory<char>>(f).data = 'c';
+	Multifactory<int, double, char> f (7, 9.0, 'c');
+
 }
 
 
