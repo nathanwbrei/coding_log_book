4d0fb0646a528a9a6f29a8cfc7225f2d6ad6ebe4
Nathan Brei <nbrei@jlab.org>
2020-06-07 03:30:47 -0400

    Delegating all the hard pieces to std::tuple

--- 2020-06-06-multifactory/main.cpp
+++ 2020-06-06-multifactory/main.cpp
@@ -1,103 +1,53 @@
 #include <iostream>
 #include <vector>
 
-/// TODO:
-/// 1. Assert that the types are unique
-/// 2. Make sense of this: https://stackoverflow.com/questions/3273504/is-it-possible-to-transform-the-types-in-a-parameter-pack
-/// 3.
 
-/// Base case
 template <typename... Ts>
 struct Multifactory {
+
+	std::tuple<std::vector<Ts*>...> data;
+
+	void set(std::vector<Ts*>... ts) {
+		data = std::make_tuple(ts...);
+	}
+
+	template<typename T>
+	std::vector<T*>& get() {
+		return std::get<std::vector<T*>>(data);
+	}
+
+	template <int level = 0>
 	void print() {
+		auto x = std::get<level>(data);
+		for (int i=0; i<x.size(); ++i) {
+			std::cout << *(x[i]) << ", ";
+		}
+		std::cout << "; ";
+		print<level+1>();
+	}
+
+	template <>
+	void print<sizeof...(Ts)>() {
 		std::cout << std::endl;
-	};
-	void set_all() {};
-
-	template <typename U>
-	U& get() {
-		// TODO: Want a compile-time error instead
-	 	throw std::runtime_error("Not found!");
 	}
-
-	template <typename U>
-	void set(U u) {
-		throw std::runtime_error("Not found!");
-	}
-
 };
-
-/// Specialization for non-empty template parameters
-template <typename T, typename... Ts>
-struct Multifactory<T, Ts...> {
-	T data;
-	Multifactory<Ts...> f;
-
-	explicit Multifactory(T t, Ts... ts) : f(ts...), data(t) {}
-
-
-	void print() {
-		std::cout << data << ", ";
-		f.print();
-	}
-
-	void set_all(T t, Ts... ts) {
-		data = t;
-		f.set_all(ts...);
-	}
-
-	void set_all(T* t, Ts*... us) {
-		data = *t;
-		f.set_all(us...);
-	}
-
-	template <typename U>
-	U& get() {
-		return f.template get<U>();
-	}
-
-	template <>
-	T& get() {
-		return data;
-	}
-
-	template <typename U>
-	void set(U u) {
-		f.template set<U>(u);
-	}
-
-	template <>
-	void set(T t) {
-		data = t;
-	}
-
-};
-
 
 
 int main() {
 	std::cout << "Hello world from multifactory" << std::endl;
-	Multifactory<int, double, char> f (7, 9.0, 'c');
+	Multifactory<int, double, char> f;
 
 	f.print();
-	f.set_all(8, 10.0, 'd');
-
-	int x = f.get<int>();
-	double y = f.get<double>();
-	char z = f.get<char>();
-	//char a = f.get<float>(); // runtime error
-
-	std::cout << x << ", " << y << ", " << z << std::endl;
-
-	f.set<double>(22.2);
+	std::vector<int*> xs = {new int(22), new int(1)};
+	std::vector<double*> ys = {new double(19.9), new double(1.0)};
+	std::vector<char*> zs = {new char('x'), new char('y')};
+	f.set(xs, ys, zs);
 	f.print();
 
-	int xx = 77;
-	double yy = 19.9;
-	char zz = 'w';
+	auto xss = f.get<int>();
+	auto zss = f.get<char>();
 
-	f.set_all(&xx, &yy, &zz);
-	f.print();
+	std::cout << *xss[0] << ", " << *xss[1] << "; " << *zss[0] << ", " << *zss[1] << std::endl;
 
 }
 
