8fb843b826945515201e313f2ef6a6244225ec70
Nathan Brei <nbrei@jlab.org>
2020-07-29 01:16:06 -0400

    Add bitmask matching algorithm

--- 2020-07-28-protoecal-cosmics-tracking/ProtoECalTrackFactory.cc
+++ 2020-07-28-protoecal-cosmics-tracking/ProtoECalTrackFactory.cc
@@ -30,10 +30,26 @@ void ProtoECalTrackFactory::Process(const std::shared_ptr<const JEvent> &event) 
     /// JFactories are local to a thread, so we are free to access and modify
     /// member variables here. However, be aware that events are _scattered_ to
     /// different JFactory instances, not _broadcast_: this means that JFactory 
-    /// instances only see _some_ of the events. 
-    
-    /// Acquire inputs (This may recursively call other JFactories)
-    auto hits = event->Get<ProtoECalHit>();
+    /// instances only see _some_ of the events.
+
+
+    // Initialize pixel grid
+    bool pixels[GRID_HEIGHT][GRID_WIDTH];
+    for (int i=0; i<GRID_HEIGHT; ++i) {
+    	for (int j=0; j<GRID_WIDTH; ++j) {
+    		pixels[i][j] = false;
+    	}
+    }
+
+    // Update pixel grid with hit information
+	auto hits = event->Get<ProtoECalHit>();
+	for (auto hit : hits) {
+		assert(hit->row >= 0);
+		assert(hit->col >= 0);
+		assert(hit->row < 3);
+		assert(hit->col < 3);
+		pixels[hit->row][hit->col] = true;
+	}
 
 	/// Do some computation
 	ProtoECalTrack* track = new ProtoECalTrack(hits);
@@ -43,3 +59,37 @@ void ProtoECalTrackFactory::Process(const std::shared_ptr<const JEvent> &event) 
     results.push_back(track);
     Set(results);
 }
+
+
+bool ProtoECalTrackFactory::test_for_tracks(bool* pixel_grid[GRID_HEIGHT][GRID_WIDTH]) {
+	//    +-+-+-+
+	//    |1|2|3|
+	//    +-+-+-+
+	//    |4|5|6|
+	//    +-+-+-+
+	//    |7|8|9|
+	//    +-+-+-+
+
+	// Convert the pixel grid into a bitset so that we can quickly compare all acceptable patterns
+	uint32_t bitset = 0;
+	for (uint32_t row=0; row<GRID_HEIGHT; row++) {
+		for (uint32_t col=0; col<GRID_WIDTH; col++) {
+			bitset |= (1u << (row*3 + col));
+		}
+	}
+
+	// Patterns are all known ahead of time
+	const uint32_t patterns[13] = {0b100'100'100, 0b010'010'010, 0b001'001'001,  // Verticals
+							       0b100'010'001, 0b001'010'100, // Diagonals
+							       0b100'010'010, 0b010'100'100, 0b010'001'001, 0b001'010'010,
+							       0b001'010'001, 0b100'010'100, 0b010'100'010, 0b010'001'010};
+
+	// Return true on the first matching pattern we find
+	for (uint32_t pattern : patterns) {
+		if ((bitset & pattern) != 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
--- 2020-07-28-protoecal-cosmics-tracking/ProtoECalTrackFactory.h
+++ 2020-07-28-protoecal-cosmics-tracking/ProtoECalTrackFactory.h
@@ -9,12 +9,16 @@
 class ProtoECalTrackFactory : public JFactoryT<ProtoECalTrack> {
 
     // Insert any member variables here
+    static const size_t GRID_HEIGHT = 3;
+	static const size_t GRID_WIDTH = 3;
 
 public:
     ProtoECalTrackFactory() : JFactoryT<ProtoECalTrack>(NAME_OF_THIS) {};
     void Init() override;
     void ChangeRun(const std::shared_ptr<const JEvent> &event) override;
     void Process(const std::shared_ptr<const JEvent> &event) override;
+
+	bool test_for_tracks(bool* pixel_grid[GRID_HEIGHT][GRID_WIDTH]);
 
 };
 
